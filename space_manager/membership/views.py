7bd53bf38d686684302a6ff9add0aa3475e2ec40
yoojat <yooja.ty@gmail.com>
2018-01-17 19:50:09 +0900

    Creating List All Memberships View

--- space_manager/membership/views.py
+++ space_manager/membership/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models, serializers
 
-# Create your views here.
+class ListAllMemberships(APIView):
+
+    def get(self, request, format=None):
+
+        all_memberships = models.Membership.objects.all()
+
+        serializer = serializers.MembershipSerializer(all_images, many=True)
+
+        return Response(data=serializer.data)
