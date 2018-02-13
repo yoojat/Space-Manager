e021fc52d09b3333fbf1b914ffe50e1f14299686
yoojat <yooja.ty@gmail.com>
2018-02-13 13:33:28 +0900

    Adding CSS Modules to Webpack

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -179,6 +179,10 @@ module.exports = {
                       options: {
                         importLoaders: 1,
                         minimize: true,
+                        modules: true,
+                        localIdentName:
+                          '[path][name]__[local]--[hash:base64:5]',
+
                         sourceMap: shouldUseSourceMap,
                       },
                     },
