b415d62365ecaf2af5518ae63a2670e5ff35223c
yoojat <yooja.ty@gmail.com>
2018-02-13 17:32:58 +0900

    Changing the NODE_PATH

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import React, {Component} from 'react';
-import styles from './App.scss';
+import styles from 'App.scss';
 
 class App extends Component {
   render() {
-    return (
-      <div className={styles.App}>
-        <header className={styles.App_header}>
-          <h1 className={styles.App_title}>Welcome to React</h1>
-        </header>
-        <p className={styles.App_intro}>To get started, whatever</p>
-      </div>
-    );
+    return <div className={styles.App} />;
   }
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import store from 'redux/configureStore';
+import 'index.css';
+import App from 'App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(store.getState());
 
-localStorage.setItem('bestCourse', 'nomad academy');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
