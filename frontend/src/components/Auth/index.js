622a5a607bfcb61219bc7ea9f169a8105cc2339f
yoojat <yooja.ty@gmail.com>
2018-02-15 18:13:32 +0900

    Finishing Auth Component and the Auth Forms

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,12 @@
+import {connect} from 'react-redux';
+import Container from './container';
+
+// Add all teh actions for
+// Login
+// Sign up
+// Recover password
+// Check username
+// Check password
+// Check email
+
+export default connect()(Container);
