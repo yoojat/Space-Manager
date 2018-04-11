a91802663b9abaf5a8be885c7dfc2e21f0a33684
yoojat <yooja.ty@gmail.com>
2018-04-11 22:25:12 +0900

    datetimepicker ok!

--- frontend/src/components/SelectWhen/container.js
+++ frontend/src/components/SelectWhen/container.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import SelectWhen from './presenter';
+import moment from 'moment';
 
 class Container extends Component {
   state = {};
 
+  _onChange(event) {
+    const datetime = event._d;
+    console.log(moment(datetime).format('YYYY-MM-DD HH:mm:ss'));
+  }
+
   render() {
-    return <SelectWhen />;
+    return <SelectWhen onChange={this._onChange} />;
   }
 }
 
--- frontend/src/components/SelectWhen/presenter.js
+++ frontend/src/components/SelectWhen/presenter.js
@@ -1,31 +1,90 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
-import {DatetimePicker} from 'rc-datetime-picker';
-import 'rc-datetime-picker/dist/picker.css';
+import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
 import moment from 'moment';
 
-class SelectWhen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      moment: moment(),
-    };
-  }
+const SelectWhen = (props, context) => {
+  moment.locale('kr', {
+    months: '1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split('_'),
+    monthsShort: '1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split(
+      '_'
+    ),
+    monthsParseExact: true,
+    weekdays: '일요일_월요일_화요일_수요일_목요일_금요일_토요일'.split('_'),
+    weekdaysShort: '일_월_화_수_목_금_토'.split('_'),
+    weekdaysMin: '일_월_화_수_목_금_토'.split('_'),
+    weekdaysParseExact: true,
+    longDateFormat: {
+      LT: 'HH:mm',
+      LTS: 'HH:mm:ss',
+      L: 'DD/MM/YYYY',
+      LL: 'D MMMM YYYY',
+      LLL: 'D MMMM YYYY HH:mm',
+      LLLL: 'dddd D MMMM YYYY HH:mm',
+    },
+    calendar: {
+      lastDay: '[Yesterday at] LT',
+      sameDay: '[Today at] LT',
+      nextDay: '[Tomorrow at] LT',
+      lastWeek: '[last] dddd [at] LT',
+      nextWeek: 'dddd [at] LT',
+      sameElse: 'L',
+    },
+    relativeTime: {
+      future: 'in %s',
+      past: '%s ago',
+      s: 'a few seconds',
+      ss: '%d seconds',
+      m: 'a minute',
+      mm: '%d minutes',
+      h: 'an hour',
+      hh: '%d hours',
+      d: 'a day',
+      dd: '%d days',
+      M: 'a month',
+      MM: '%d months',
+      y: 'a year',
+      yy: '%d years',
+    },
+    dayOfMonthOrdinalParse: /\d{1,2}(er|e)/,
+    ordinal: function(number) {
+      return number + (number === 1 ? 'er' : 'e');
+    },
+    meridiemParse: /PD|MD/,
+    isPM: function(input) {
+      return input.charAt(0) === 'M';
+    },
+    // In case the meridiem units are not separated around 12, then implement
+    // this function (look at locale/id.js for an example).
+    // meridiemHour : function (hour, meridiem) {
+    //     return /* 0-23 hour, given meridiem token and hour 1-12 */ ;
+    // },
+    meridiem: function(hours, minutes, isLower) {
+      return hours < 12 ? 'AM' : 'PM';
+    },
+    week: {
+      dow: 1, // Monday is the first day of the week.
+      doy: 4, // The week that contains Jan 4th is the first week of the year.
+    },
+  });
 
-  handleChange = moment => {
-    this.setState({
-      moment,
-    });
-  };
+  return (
+    <Fragment>
+      <div>이용일수를 선택해 주세요</div>
+      <Datetime
+        dateFormat="YYYY-MM-DD"
+        timeFormat="a hh:mm"
+        onChange={props.onChange}
+        className={styles.datetime}
+        defaultValue={new Date()}
+      />
+    </Fragment>
+  );
+};
 
-  render() {
-    return (
-      <DatetimePicker moment={this.state.moment} onChange={this.handleChange} />
-    );
-  }
-}
 SelectWhen.propTypes = {};
 
 export default SelectWhen;
